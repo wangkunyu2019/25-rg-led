ccac440368fa972bb7da81b84aeb428916e7080d
wangkunyu2019 <2019040262@qq.com>
2026-01-11 11:27:58 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(1000)
+})
